6eb1c02658badbb6566c16747f794a16fb6127b7
Enrique <ejsv1899@gmail.com>
2024-03-21 19:43:59 -0600

    ejercicio 3.3 completado

--- index.js
+++ index.js
@@ -43,6 +43,19 @@ app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
 
+// get one person
+app.get("/api/persons/:id", (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+    
+    // if exists
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
